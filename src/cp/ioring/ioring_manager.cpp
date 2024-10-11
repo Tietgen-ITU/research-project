f385e51ba7675fdc6577c5e54ba33b0fbd3b16eb
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-10-11 08:35:58 +0000

    Add setup for io_uring lib

---
+++ src/cp/ioring/ioring_manager.cpp
@@ -0,0 +1,20 @@
+#include "../include/ioring/ioring_manager.hpp"
+
+namespace ioring
+{
+    IORingManager::IORingManager()
+    {
+    }
+
+    IORingManager::~IORingManager()
+    {
+    }
+
+    void IORingManager::submit_read_request()
+    {
+    }
+
+    void IORingManager::submit_write_request()
+    {
+    }
+}
