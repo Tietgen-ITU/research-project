8529f321217bbbc5a71bba5d7b28781198e5a788
emiloh <Emil@houlborg.dk>
2024-12-09 16:06:55 +0100

    feat: Add plotting for completion latency

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -6,20 +6,26 @@ from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
 
-Results: TypeAlias = DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str,float]]]]]]
+Results: TypeAlias = DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str, DefaultDict[str,float]]]]]]]
 
 def parse_result(folder: Path) -> Results:
     # name -> bs -> precon_util -> io_depth -> threads
-    results = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float)))))) 
+    results = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(float)))))))
     for path in folder.rglob("*.txt"):
         with open(path, "r") as f:
             run = json.load(f)
-            name, bs, precon_util, iodepth, threads  = (path.stem).split("_")
-            jobs = run["jobs"][0]
-            iops = float(jobs["write"]["iops_mean"])
-            iopss_stdv = float(jobs["write"]["iops_stddev"])
-            results[name][bs][precon_util][iodepth][threads]["iops"] = iops
-            results[name][bs][precon_util][iodepth][threads]["iops_stddev"] = iopss_stdv
+            print(path.stem)
+            test, name, bs, precon_util, iodepth, threads  = (path.stem).split("_")
+            read_job = run["jobs"][0]["read"]
+            write_job = run["jobs"][0]["write"]
+            
+            # Read clat 99th percentile
+            results[name]["read"][bs][precon_util][iodepth][threads]["clat"] = read_job["clat_ns"]["percentile"]["99.000000"]
+            results[name]["write"][bs][precon_util][iodepth][threads]["clat"] = write_job["clat_ns"]["percentile"]["99.000000"]
+
+            # Read iops for Writes
+            results[name]["write"][bs][precon_util][iodepth][threads]["iops"] = write_job["iops_mean"]
+            results[name]["write"][precon_util][iodepth][threads]["iops_stddev"] = write_job["iops_stddev"]
     return results
             
 
@@ -43,10 +49,10 @@ def plot_iops_iodepth_threads(results: Results, bs: str, precon: str) -> None:
     hathces = ["//", "--", "xx", "++"]
     
     # Gather lists from results
-    nonfdp_iodepth = [results["nonfdp"][bs][precon][str(tio)]["1"]["iops"] for tio in x_ticks]    
-    fdp_iodepth = [results["fdp"][bs][precon][str(tio)]["1"]["iops"] for tio in x_ticks]
-    nonfdp_threads = [results["nonfdp"][bs][precon]["1"][str(tio)]["iops"] for tio in x_ticks]
-    fdp_threads = [results["nonfdp"][bs][precon]["1"][str(tio)]["iops"] for tio in x_ticks]
+    nonfdp_iodepth = [results["nonfdp"]["write"][bs][precon][str(tio)]["1"]["iops"] for tio in x_ticks]    
+    fdp_iodepth = [results["fdp"]["write"][bs][precon][str(tio)]["1"]["iops"] for tio in x_ticks]
+    nonfdp_threads = [results["nonfdp"]["write"][bs][precon]["1"][str(tio)]["iops"] for tio in x_ticks]
+    fdp_threads = [results["fdp"]["write"][bs][precon]["1"][str(tio)]["iops"] for tio in x_ticks]
     l = [("Non-FDP (iodepth)", nonfdp_iodepth), ("FDP (iodepth)", fdp_iodepth)]#, ("Non-FDP (threads)", nonfdp_threads), ("FDP (threads)", fdp_threads)]
     for i, (case, measurements) in enumerate(l):
         offset = bar_width * multiplier
@@ -63,13 +69,45 @@ def plot_iops_iodepth_threads(results: Results, bs: str, precon: str) -> None:
     plt.savefig("iops_iodepth_threads.pdf", format="pdf", bbox_inches="tight")
 
 
+def plot_clat(results, bs, precon):
+    threads = ["1", "2", "4", "8", "16"]
+    io_depth = ["1", "2", "4"]
 
+    colors = ["blue", "green", "violet", "orange"]
+    markers = [".", "x", "s", "v"]
+    linestyles = ["solid", "dotted", "solid", "dotted"]
+
+    for thread in threads:
+        
+        fig, ax = plt.subplots()
+        
+        write_nonfdp = [results["nonfdp"]["write"][bs][precon][iodep][thread]["clat"] / 1000 for iodep in io_depth]
+        write_fdp = [results["fdp"]["write"][bs][precon][iodep][thread]["clat"] / 1000 for iodep in io_depth]
+        read_nonfdp = [results["nonfdp"]["read"][bs][precon][iodep][thread]["clat"] / 1000 for iodep in io_depth]
+        read_fdp = [results["nonfdp"]["read"][bs][precon][iodep][thread]["clat"] / 1000 for iodep in io_depth]
+        
+        l = [("Write", write_nonfdp), ("Write - FDP", write_fdp), ("Read", read_nonfdp), ("Read - FDP", read_fdp)] 
+        
+        for i, (lbl, res) in enumerate(l): 
+            plt.plot(io_depth, res, color=colors[i], marker=markers[i], linestyle=linestyles[i], label=lbl)
+        
+        plt.ylabel("Completion latency (microseconds)")
+        plt.xlabel("IO depth")
+        plt.legend(loc="upper right", ncols=1)
+
+        plt.savefig(f"clat_threads{thread}.pdf", format="pdf", bbox_inches="tight")
+
+
+
+
+
+    
 
 
 def main(folder: Path) -> None:
     results = parse_result(folder)
-    plot_iops_iodepth_threads(results, "4096", "0")
-    print(results)
+    #plot_iops_iodepth_threads(results, "4096", "0")
+    plot_clat(results, "4096", "0")
     
 if __name__ == "__main__":
     folder = Path(sys.argv[1])
